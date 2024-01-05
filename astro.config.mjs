2d921ac639dff40ad138eb5563aa301f2dfe98b1
Max Shen <m4xshen@gmail.com>
2024-01-05 22:17:40 +0800

    build: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://m4xshen.dev',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
