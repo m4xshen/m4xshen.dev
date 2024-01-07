21d17ad1b151bc759a5b5c8efa3890d1bb8b5dda
Max Shen <m4xshen@gmail.com>
2024-01-07 14:30:30 +0800

    feat: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
 
 export default defineConfig({
 	site: 'https://m4xshen.dev',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
