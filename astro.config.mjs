11d9d2289715a3d6682b8dddbeac879f7c5449f4
Max Shen <m4xshen@gmail.com>
2024-01-07 13:51:52 +0800

    feat: add og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://m4xshen.dev',
 	integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
+  },
 });
