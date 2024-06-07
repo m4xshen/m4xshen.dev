d1b3d89d38a18bce257953972b2498e5f9f0e524
Max Shen <m4xshen@gmail.com>
2024-06-07 17:16:15 +0800

    refactor: refine url format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-	site: 'https://m4xshen.dev',
-	integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-  ],
+  site: "https://m4xshen.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
     },
   },
   redirects: {
-    '/posts': '/',
+    "/posts": "/",
   },
+  trailingSlash: "never",
 });
