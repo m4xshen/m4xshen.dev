38e090152a40f2c2d12223e4610ea2489b959e76
Max Shen <m4xshen@gmail.com>
2026-04-17 16:39:58 -0500

    chore: upgrade astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'zod';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
-  // Type-check frontmatter using a schema
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     ogImage: z.string().optional(),
